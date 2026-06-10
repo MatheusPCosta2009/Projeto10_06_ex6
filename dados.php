a78457a41cf350d3729b0e1a38641e8a5fa540aa
MatheusPCosta2009 <mathpercosta@gmail.com>
2026-06-10 11:06:51 -0300

    primeiro commit

--- dados.php
+++ dados.php
@@ -9,89 +9,14 @@
 <body>
     <h1>Página de processamento</h1>
     <?php
-        $estado = $_POST["estado"];
-        switch($estado){
-            case "Acre":
-                echo "Rio Branco";
-                break;
-            case "Alagoas":
-                echo "Maceió";
-                break;
-            case "Amapá":
-                echo "Macapá";
-                break;
-            case "Amazonas":
-                echo "Manaus";
-                break;
-            case "Bahia":
-                echo "Salvador";
-                break;
-            case "Ceará":
-                echo "Fortaleza";
-                break;
-            case "Distrito Federal":
-                echo "Brasília";
-                break;
-            case "Goiás":
-                echo "Goiânia";
-                break;
-            case "Espírito Santo":
-                echo "Vitória";
-                break;
-            case "Maranhão":
-                echo "São Luís";
-                break;
-            case "Mato Grosso":
-                echo "Cuiabá";
-                break;
-            case "Mato Grosso do Sul":
-                echo "Campo Grande";
-                break;
-            case "Minas Gerais":
-                echo "Belo Horizonte";
-                break;
-            case "Pará":
-                echo "Belém";
-                break;
-            case "Paraíba":
-                echo "João Pessoa";
-                break;
-            case "Paraná":
-                echo "Curitiba";
-                break;
-            case "Pernambuco":
-                echo "Recife";
-                break;
-            case "Piauí":
-                echo "Teresina";
-                break;
-            case "Rio de Janeiro":
-                echo "Rio de Janeiro";
-                break;
-            case "Rio Grande do Norte":
-                echo "Natal";
-                break;
-            case "Rio Grande do Sul":
-                echo "Porto Alegre";
-                break;
-            case "Rondônia":
-                echo "Porto Velho";
-                break;
-            case "Roraima":
-                echo "Boa Vista";
-                break;
-            case "Santa Catarina":
-                echo "Florianópolis";
-                break;
-            case "São Paulo":
-                echo "São Paulo";
-                break;
-            case "Sergipe":
-                echo "Aracaju";
-                break;
-            case "Tocantins":
-                echo "Palmas";
-                break;
+        $ano = $_POST["ano"];
+        $checagem1 = $ano % 4;
+        $checagem2 = $ano & 100;
+        if($checagem1 == 0 && $checagem2 != 0){
+            echo "Ano bissexto";
+        }
+        else{
+            echo "Ano comum";
         }
     ?>
 </body>
